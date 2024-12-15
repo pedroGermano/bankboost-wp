41fa1f892f7c5654fda92eafefa1fdeee4b570e6
Pedro Germano <pedrogermano232@gmail.com>
2024-12-15 18:31:14 -0300

    ok

---
+++ page-home.php
@@ -0,0 +1,257 @@
+<?php
+  // Template name: Home
+?>
+<?php get_header(); ?>
+    <section class="s-hero">
+      <div class="container">
+        <div class="left" data-aos="fade-right">
+          <h3>Abra sua conta, é só baixar o app!</h3>
+          <h1>Desperte seu potencial financeiro com o Bankboost!</h1>
+          <a href="#" class="btn">Abra sua conta digital</a>
+          <ul>
+            <li>
+              <img src="<?php echo get_template_directory_uri() ?>/img/icon-1.svg" alt="icon" />
+              <span>Cartão sem <br />anuidade</span>
+            </li>
+            <li>
+              <img src="<?php echo get_template_directory_uri() ?>/img/icon-2.svg" alt="icon" />
+              <span>Conta digital<br />100% grátis</span>
+            </li>
+            <li>
+              <img src="<?php echo get_template_directory_uri() ?>/img/icon-3.svg" alt="icon" />
+              <span>Seu dinheiro <br />rendendo mais</span>
+            </li>
+          </ul>
+        </div>
+        <div class="right" data-aos="fade-left">
+          <h2>Banco 100% digital</h2>
+          <div class="cards">
+            <img class="item card-one" src="<?php echo get_template_directory_uri() ?>/img/card-1.png" alt="card 01" />
+            <img class="item card-two" src="<?php echo get_template_directory_uri() ?>/img/card-2.png" alt="card 02" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="s-boost">
+      <div class="container">
+        <div class="left" data-aos="fade-up">
+          <div class="mockup">
+            <img src="<?php echo get_template_directory_uri() ?>/img/app.svg" alt="Mockup" />
+          </div>
+          <div class="description">
+            <img class="icon" src="<?php echo get_template_directory_uri() ?>/img/icon-7.svg" alt="boost" />
+            <h3>Baixe nosso app</h3>
+            <p>
+              Que tal abrir uma conta digital para ver como a gente faz a sua
+              vida muito mais simples?
+            </p>
+            <ul>
+              <li>
+                <a href="#" target="_blank">
+                  <img src="<?php echo get_template_directory_uri() ?>/img/button-ios.svg" alt="button apple" />
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank">
+                  <img src="<?php echo get_template_directory_uri() ?>/img/button-android.svg" alt="button google" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="right" data-aos="fade-left">
+          <h2>Resolva sua vida direto pelo app boost!</h2>
+          <ul class="list">
+            <li>
+              <div class="icon">
+                <img src="<?php echo get_template_directory_uri() ?>/img/icon-credit-card.svg" alt="icon card" />
+              </div>
+              <div class="text">
+                <h4>Cartão de crédito sem anuidade</h4>
+                <p>
+                  Conta digital com cartão de crédito sem anuidade e sem
+                  complicação
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <img src="<?php echo get_template_directory_uri() ?>/img/icon-porcent.svg" alt="icon card" />
+              </div>
+              <div class="text">
+                <h4>Sem taxas</h4>
+                <p>
+                  Transferências, boletos de depósito e outros serviços
+                  gratuitos
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <img src="<?php echo get_template_directory_uri() ?>/img/icon-cifrao.svg" alt="icon card" />
+              </div>
+              <div class="text">
+                <h4>Mais investimentos</h4>
+                <p>Rendem mais que a poupança e você resgata quando quiser</p>
+              </div>
+            </li>
+          </ul>
+          <a href="#" class="btn">Conheça outros produtos</a>
+          <div class="box">
+            <div class="text">
+              <h3>Boost Pejota</h3>
+              <h5>Contas digitais PJ gratuitas para decolar seu negócio!</h5>
+              <p>
+                As melhores contas para fazer pagamentos, compras e receber dos
+                seus clientes.
+              </p>
+              <ul>
+                <li>
+                  <button>Sou MEI</button>
+                </li>
+                <li>
+                  <button>Sou ME</button>
+                </li>
+              </ul>
+            </div>
+            <img class="card" src="<?php echo get_template_directory_uri() ?>/img/card.svg" alt="card" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="s-depositions">
+      <div class="container" data-aos="fade-down">
+        <div class="title">
+          <h2>
+            Prático, fácil, moderno
+            <strong>Você resolve tudo sem estresse 😊</strong>
+          </h2>
+        </div>
+
+        <div class="slide-depositions">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <div class="card-deposition">
+                <div class="user">
+                  <span>@marciogurka</span>
+                  <img src="<?php echo get_template_directory_uri() ?>/img/twitter-icon.svg" alt="twitter icon" />
+                </div>
+                <p>
+                  Valeu @banconenon! Linda embalagem e o cartão tbm é lindo!
+                  Muito amor por esse banco #bancoNeon
+                </p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-deposition">
+                <div class="user">
+                  <span>@marciogurka</span>
+                  <img src="<?php echo get_template_directory_uri() ?>/img/twitter-icon.svg" alt="twitter icon" />
+                </div>
+                <p>
+                  Valeu @banconenon! Linda embalagem e o cartão tbm é lindo!
+                  Muito amor por esse banco #bancoNeon
+                </p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-deposition">
+                <div class="user">
+                  <span>@marciogurka</span>
+                  <img src="<?php echo get_template_directory_uri() ?>/img/twitter-icon.svg" alt="twitter icon" />
+                </div>
+                <p>
+                  Valeu @banconenon! Linda embalagem e o cartão tbm é lindo!
+                  Muito amor por esse banco #bancoNeon
+                </p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-deposition">
+                <div class="user">
+                  <span>@marciogurka</span>
+                  <img src="<?php echo get_template_directory_uri() ?>/img/twitter-icon.svg" alt="twitter icon" />
+                </div>
+                <p>
+                  Valeu @banconenon! Linda embalagem e o cartão tbm é lindo!
+                  Muito amor por esse banco #bancoNeon
+                </p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-deposition">
+                <div class="user">
+                  <span>@marciogurka</span>
+                  <img src="<?php echo get_template_directory_uri() ?>/img/twitter-icon.svg" alt="twitter icon" />
+                </div>
+                <p>
+                  Valeu @banconenon! Linda embalagem e o cartão tbm é lindo!
+                  Muito amor por esse banco #bancoNeon
+                </p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="card-deposition">
+                <div class="user">
+                  <span>@marciogurka</span>
+                  <img src="<?php echo get_template_directory_uri() ?>/img/twitter-icon.svg" alt="twitter icon" />
+                </div>
+                <p>
+                  Valeu @banconenon! Linda embalagem e o cartão tbm é lindo!
+                  Muito amor por esse banco #bancoNeon
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="s-account">
+      <div class="container">
+        <div class="left" data-aos="fade-right">
+          <h2>
+            Aproveite
+            <strong>Domine o seu dinheiro com uma conta 100% digital</strong>
+          </h2>
+          <ul>
+            <li>
+              <img src="<?php echo get_template_directory_uri() ?>/img/icon-square-1.svg" alt="Icon card" />
+              <div class="text">
+                <h4>Cartão visa internacional</h4>
+                <p>Tenha facilidades e benefícios para o seu dia a dia.</p>
+              </div>
+            </li>
+            <li>
+              <img src="<?php echo get_template_directory_uri() ?>/img/icon-square-2.svg" alt="Icon card" />
+              <div class="text">
+                <h4>Zero mensalidade e anuidade</h4>
+                <p>Não gaste grana pagando taxas desnecessárias.</p>
+              </div>
+            </li>
+            <li>
+              <img src="<?php echo get_template_directory_uri() ?>/img/icon-square-3.svg" alt="Icon card" />
+              <div class="text">
+                <h4>Investimento que rende mais que a poupança</h4>
+                <p>Invista o seu dinheiro de maneira mais rentável.</p>
+              </div>
+            </li>
+          </ul>
+          <a href="#" class="btn">Abra sua conta digital</a>
+        </div>
+        <div class="right">
+          <img
+            class="mockup one"
+            data-aos="fade-up"
+            src="<?php echo get_template_directory_uri() ?>/img/mockup1.png"
+            alt="mock 1"
+          />
+          <img
+            class="mockup two"
+            data-aos="fade-down"
+            src="<?php echo get_template_directory_uri() ?>/img/mockup2.png"
+            alt="mock 2"
+          />
+        </div>
+      </div>
+    </section>
+<?php get_footer(); ?>
